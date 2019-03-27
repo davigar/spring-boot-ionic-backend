95a7de1eec215f9b9a77913367aa0de56aa4aaa3
Davi Gardin <davi.gardin@gmail.com>
2019-03-27 07:11:54 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/example/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/example/cursomc/dto/CategoriaDTO.java
@@ -2,18 +2,20 @@ package com.example.cursomc.dto;
 
 import java.io.Serializable;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.example.cursomc.domain.Categoria;
 
 public class CategoriaDTO  implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5,max=80, message="O tamanho deve ser entre 5 e 80 caracteres.")
 	private String nome;
 	
 	public CategoriaDTO() {

13a2339cadf1d1b289cc01dbbf76adec548ff200
Davi Gardin <davi.gardin@gmail.com>
2019-04-08 08:01:29 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/example/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/example/cursomc/dto/ClienteDTO.java
@@ -8,26 +8,27 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.example.cursomc.domain.Cliente;
+import com.example.cursomc.services.validation.ClienteUpdate;
 
-public class ClienteDTO   implements Serializable{
+@ClienteUpdate
+public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
-	
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Email(message="E-mail inválido")
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "E-mail inválido")
 	private String email;
 
-	
 	public ClienteDTO() {
-	
+
 	}
-	
+
 	public ClienteDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();

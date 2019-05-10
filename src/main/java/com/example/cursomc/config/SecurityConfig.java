a5ae13c01f8207137aded4704743f18ab315925b
Davi Gardin <davi.gardin@gmail.com>
2019-05-10 07:38:19 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/example/cursomc/config/SecurityConfig.java
+++ src/main/java/com/example/cursomc/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATHCERS = { "/h2-console/**"};
 	
-	private static final String[] PUBLIC_MATHCERS_GET = { "/produtos/**", "/categorias/**" };
+	private static final String[] PUBLIC_MATHCERS_GET = { "/produtos/**", "/categorias/**", "/clientes/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {

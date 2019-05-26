5e62599a8c877eff536e97377e4ec612ae600230
Davi Gardin <davi.gardin@gmail.com>
2019-05-26 07:51:36 -0300

    Autorizando endpoint para perfis especificos

--- src/main/java/com/example/cursomc/config/SecurityConfig.java
+++ src/main/java/com/example/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,20 +25,23 @@ import com.example.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private JWTUtil jwtUtil;
-	
+
 	private static final String[] PUBLIC_MATHCERS = { "/h2-console/**" };
 
 	private static final String[] PUBLIC_MATHCERS_GET = { "/produtos/**", "/categorias/**" };
+
+	private static final String[] PUBLIC_MATHCERS_POST = { "/clientes/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -46,8 +50,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATHCERS_GET).permitAll()
-				.antMatchers(PUBLIC_MATHCERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATHCERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATHCERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATHCERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);

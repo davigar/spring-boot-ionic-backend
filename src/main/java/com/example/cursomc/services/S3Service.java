f0fbd53e90fc2a98d18916316526913888e98de5
Davi Gardin <davi.gardin@gmail.com>
2019-06-12 06:09:57 -0300

    Enviando imagem via Endpoint

--- src/main/java/com/example/cursomc/services/S3Service.java
+++ src/main/java/com/example/cursomc/services/S3Service.java
@@ -5,8 +5,6 @@ import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import javax.management.RuntimeErrorException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.example.cursomc.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -55,7 +52,7 @@ public class S3Service {
 
 			return s3client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao conerter URL para URI");
+			throw new FileException("Erro ao conerter URL para URI");
 		}
 
 	}
